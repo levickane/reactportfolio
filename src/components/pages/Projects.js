54720579c862ea3bc84edfb4df9d3d1d481214c4
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-06 09:31:56 -0500

    initial project data is rendering to the page via json file. next is to populate the json file with everything from the previous portfolio website

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,24 +1,22 @@
 import React from 'react';
+import projectData from '../../projectData.json';
 
 function Projects() {
   return (
     <div>
-      <h1>Projects Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <h1>My projects</h1>
+      {projectData.map((project) => (
+        <div className="card" key={project.id}>
+          {/* <img src="..." className="card-img-top" alt="..." /> */}
+          <div className="card-body">
+            <h5 className="card-title">{project.title}</h5>
+            <p className="card-text">{project.subtitle}</p>
+            {/* <a href= className="btn btn-primary">
+              Go somewhere
+            </a> */}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
