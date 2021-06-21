5c30b366fb5b44cf68dc81c4c048e26bb42d70d1
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-21 08:22:19 -0500

    nu-seal is added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,14 +11,22 @@ function Home() {
   return (
     <div>
       <div className="card shadow rounded-0">
-        <h5 className="card-header">Who is he?</h5>
+        <div className="card-header d-flex justify-content-between">
+          <h5 className="pt-3">Levi Kane</h5>
+          <img
+            src="./logos/NU-seal.png"
+            alt="nu-seal"
+            style={{ height: '50px' }}
+          />
+        </div>
         <div className="card-body row justify-content-center">
           <p className="col-md-9">
-            Dedicated and driven individual with experience producing high
-            quality full stack code. Quick to learn, hungry to learn more, full
-            of passion and dedication, Levi will stop at nothing to deliver his
-            absolute best. With creativity and ingenuity to fuel his fire, grit
-            and hustle are two things that he cannot live without.
+            A Northwestern University Full Stack Bootcamp graduate. Dedicated
+            and driven individual with experience producing high quality full
+            stack code. Quick to learn, hungry to learn more, full of passion
+            and dedication, Levi will stop at nothing to deliver his absolute
+            best. With creativity and ingenuity to fuel his fire, grit and
+            hustle are two things that he cannot live without.
           </p>
           <img
             className=" my-auto col-md-3 col-sm-6"
